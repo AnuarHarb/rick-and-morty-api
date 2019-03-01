717de3dc3da023ec7fdbf9b034c401fbac6e2ddf
Anuar Harb <anuarharb@hotmail.com>
2019-02-28 22:01:12 -0600

    ejemplo de api

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,34 +1,59 @@
 //Querys al DOM
-const cards = document.getElementById('cards');
+const charactersContainer = document.getElementById('characters');
+// const loading = document.getElementById('loading');
+const filterButtons = document.querySelectorAll('.filter-button');
 
 //Url de API
-const apiUrl = 'https://rickandmortyapi.com/api/character/';
+const apiUrl = 'https://rickandmortyapi.com/api/character';
 
-fetch(apiUrl)
-  .then(function(response) {
-    console.log(response)
-    return response.json();
-  })
-  .then(function(respJson) {
-    const personajes = respJson.results;
-    imprimirPersonajes(personajes);
-  })
-  .catch(function(error) {
-    console.log(error);
-  })
 
-  function imprimirPersonajes(personajes){
-    for(let personaje of personajes) {
-      console.log(personaje);
-      cards.innerHTML += `
-        <p>${personaje.name}</p>
-        <img src="${personaje.image}">
-      `
-    }
+for(let button of filterButtons) {
+  button.addEventListener('click', filterCharacters);
+}
+
+function filterCharacters(event) {
+  const filter = event.target.getAttribute('data-filter');
+  const filterApi = apiUrl + '/?gender=' + filter;
+  charactersContainer.innerHTML = '';
+  apiCall(filterApi);
+}
+
+function apiCall(apiToCall){
+
+  fetch(apiToCall)
+    .then(function(resp) {
+      return resp.json();
+    })
+    .then(function(resp){
+      console.log(resp.info.next)
+      if(resp.info.next != ''){
+        apiCall(resp.info.next);
+        for(let button of filterButtons) {
+          button.disabled = true;
+        }
+      } else {
+        for(let button of filterButtons) {
+          button.disabled = false;
+        }
+      }
+      printCharacters(resp.results);
+    })
+}
+
+
+function printCharacters(characters) {
+  for(let character of characters) {
+    charactersContainer.innerHTML += `
+      <article class="card species-${character.species.toLowerCase()}">
+        <h2>${character.name}</h2>
+        <img src="${character.image}">
+        <p>${character.status}</p>
+        <p>${character.gender}</p>
+        <p>${character.species}</p>
+        <p>${character.location.name}</p>
+      </article>
+    `
   }
+}
 
-
-
-
-
-///
+apiCall(apiUrl);
